9104c2640a1057d9cfa72d1266214d5272d1934a
hellodamii <daremiarchs@gmail.com>
2024-10-02 20:07:30 +0100

    Initial Commit

---
+++ Notes App/Notes_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Notes_AppApp.swift
+//  Notes App
+//
+//  Created by Busha on 02/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Notes_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
