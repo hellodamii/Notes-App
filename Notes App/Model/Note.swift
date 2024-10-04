6305271b941e86448e51a6ddfa1079c600d7613b
hellodamii <daremiarchs@gmail.com>
2024-10-04 22:16:28 +0100

    Hello world!

---
+++ Notes App/Model/Note.swift
@@ -0,0 +1,18 @@
+//
+//  Note.swift
+//  Notes App
+//
+//  Created by Busha on 02/10/2024.
+//
+
+import SwiftUI
+
+struct Note: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Note()
+}
---
+++ Notes App/View/Home.swift
@@ -0,0 +1,18 @@
+//
+//  Home.swift
+//  Notes App
+//
+//  Created by Busha on 02/10/2024.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Home()
+}
